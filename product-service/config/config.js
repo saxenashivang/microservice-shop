fe01929d78997ff4c5d35e866a8eba093bdc0602
Shivang Saxena <shivang5199@gmail.com>
2023-05-24 17:53:18 +0530

    rabbit-mq initial setup

--- product-service/config/config.js
+++ product-service/config/config.js
@@ -1,15 +1,16 @@
-const config={
-    production :{
-        SECRET: process.env.SECRET,
-        DATABASE: process.env.MONGODB_URI
-    },
-    default : {
-        SECRET: 'mysecretkey',
-        DATABASE: 'mongodb://localhost:27017/Products'
-    }
-}
+const config = {
+  production: {
+    SECRET: process.env.SECRET,
+    DATABASE: process.env.MONGODB_URI,
+    RABBITMQ_URL: process.env.RABBITMQ_URL,
+  },
+  default: {
+    SECRET: "mysecretkey",
+    DATABASE: "mongodb://localhost:27017/Products",
+    RABBITMQ_URL: "amqp://localhost:5672",
+  },
+};
 
-
-exports.get = function get(env){
-    return config[env] || config.default
-}
+exports.get = function get(env) {
+  return config[env] || config.default;
+};
--- product-service/utils/index.js
+++ product-service/utils/index.js
@@ -1,37 +1,46 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
+const jwt  = require('jsonwebtoken');
 
-const config = require("../config/config").get(process.env.NODE_ENV);
+const config=require('../config/config').get(process.env.NODE_ENV);
 
-// Utility functions
-(module.exports.GenerateSalt = async () => await bcrypt.genSalt()),
-  (module.exports.GeneratePassword = async (password, salt) => await bcrypt.hash(password, salt);
+//Utility functions
+module.exports.GenerateSalt = async() => {
+        return await bcrypt.genSalt()    
+},
+
+module.exports.GeneratePassword = async (password, salt) => {
+        return await bcrypt.hash(password, salt);
+};
 
 
-module.exports.ValidatePassword = async (
-  enteredPassword,
-  savedPassword,
-  salt
-) => await this.GeneratePassword(enteredPassword, salt) === savedPassword;
+module.exports.ValidatePassword = async (enteredPassword, savedPassword, salt) => {
+        return await this.GeneratePassword(enteredPassword, salt) === savedPassword;
+};
 
-(module.exports.GenerateSignature = async (payload) => await jwt.sign(payload, config.SECRET, { expiresIn: '1d'} )),
-  (module.exports.ValidateSignature = async (req) => {
-    const signature = req.get("Authorization");
+module.exports.GenerateSignature = async (payload) => {
+        return await jwt.sign(payload, config.SECRET, { expiresIn: '1d'} )
+}, 
 
-    if (signature) {
-      const payload = await jwt.verify(signature.split(" ")[1], config.SECRET);
-      console.log("payload", signature);
-      req.user = payload;
-      return true;
-    }
+module.exports.ValidateSignature  = async(req) => {
 
-    return false;
-  });
+        const signature = req.get('Authorization');
+
+        console.log(signature);
+        
+        if(signature){
+            const payload = await jwt.verify(signature.split(' ')[1], config.SECRET);
+            console.log("payload",signature);
+            req.user = payload;
+            return true;
+        }
+
+        return false
+};
 
 module.exports.FormateData = (data) => {
         if(data){
             return { data }
-        }
+        }else{
             throw new Error('Data Not found!')
-        
+        }
     }
